5fe078b340d72016b09c4b34527f8babfb2f2deb
John Samarasinghe <john.samarasinghe@outlook.com>
2026-02-23 07:06:09 +0530

    Mediabox update

--- MediaBoxWebApp/Program.cs
+++ MediaBoxWebApp/Program.cs
@@ -1,3 +1,4 @@
+using MediaBoxManager;
 using MediaBoxWebApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<Manager>();
 
 var app = builder.Build();
 
